f490eb892b4de7a6dca07ecd9a47cfbcfe0ba717
k1nd0ne <k1nd0ne@mail.com>
2023-11-01 18:43:01 +0100

    Better prefetch decompression with new huffman algorithm

--- prefetch.py
+++ prefetch.py
@@ -3,149 +3,229 @@
 # https://github.com/libyal/libscca/blob/main/documentation/Windows%20Prefetch%20File%20(PF)%20format.asciidoc
 # https://github.com/volatilityfoundation/volatility3/
 # https://github.com/EricZimmerman/Prefetch/tree/master/Prefetch
-import logging, pathlib, datetime, io, numpy
+import logging, pathlib, datetime, io, struct
 from volatility3.framework import renderers, interfaces, exceptions, constants
 from volatility3.framework.configuration import requirements
 from volatility3.plugins.windows import filescan
 from volatility3.framework.renderers import format_hints, conversion
 
 vollog = logging.getLogger(__name__)
+from typing import Tuple, List, Union
+
+class BitStream:
+    def __init__(self, source: bytes, in_pos: int):
+        self.source = source
+        self.index = in_pos + 4
+        # read UInt16 little endian
+        mask = struct.unpack_from('<H', source, in_pos)[0] << 16
+        mask += struct.unpack_from('<H', source, in_pos + 2)[0]
+        self.mask = mask
+        self.bits = 32
+
+    def lookup(self, n: int) -> int:
+        if n == 0:
+            return 0
+        return self.mask >> (32 - n)
+        
+    def skip(self, n: int) -> Union[None, Exception]:
+        self.mask = ((self.mask << n) & 0xFFFFFFFF)
+        self.bits -= n
+        if self.bits < 16:
+            if self.index + 2 > len(self.source):
+                return Exception("EOF Error")
+            # read UInt16 little endian
+            self.mask += ((struct.unpack_from('<H', self.source, self.index)[0]) << (16 - self.bits)) & 0xFFFFFFFF 
+            self.index += 2
+            self.bits += 16
+
+        return None
+
+    def __str__(self):
+        return f"{self.id}: symbol {self.symbol} length {self.length}"
+class PREFIX_CODE_NODE:
+    def __init__(self):
+        self.id = 0
+        self.symbol = 0
+        self.leaf = False
+        self.child = [None, None]
+
+    def __str__(self):
+        return f"Node {self.id}: symbol {self.symbol} leaf {self.leaf}"
+
+class PREFIX_CODE_SYMBOL:
+    def __init__(self):
+        self.id = 0
+        self.symbol = 0
+        self.length = 0
+
+    def __str__(self):
+        return f"Symbol {self.id}: symbol {self.symbol} length {self.length}"
 
 
-def encoded_bit_length(data, symbol):
-    if (symbol % 2) == 0:
-        return int(data[symbol // 2] & 0x0f)
-    else:
-        return int(data[symbol // 2] >> 4)
+def prefix_code_tree_add_leaf(treeNodes: List[PREFIX_CODE_NODE], leafIndex: int, mask: int, bits: int) -> int:
+    node = treeNodes[0]
+    i = leafIndex + 1
+    childIndex = None
 
+    while bits > 1:
+        bits -= 1
+        childIndex = (mask >> bits) & 1
+        if node.child[childIndex] == None:
+            node.child[childIndex] = treeNodes[i]
+            treeNodes[i].leaf = False
+            i += 1
+        node = node.child[childIndex]
 
-def read_16_bits(input, current_position):
-    stream = io.BytesIO(input)
-    stream.seek(current_position)
-    byte_value = bytearray(stream.read(2))
-    val = numpy.uint16(0)
-    j = 0
-    for i in byte_value:
-        val = val | (numpy.uint16(i) << numpy.uint(j * 8))
-        j = j + 1
-    return val
+    node.child[mask&1] = treeNodes[leafIndex]
 
+    return i
 
-def read_byte(input, current_position):
-    stream = io.BytesIO(input)
-    stream.seek(current_position)
-    return int.from_bytes(stream.read(1), "little")
+def prefix_code_tree_rebuild(input: bytes) -> PREFIX_CODE_NODE:
+    treeNodes = [PREFIX_CODE_NODE() for _ in range(1024)]
+    symbolInfo = [PREFIX_CODE_SYMBOL() for _ in range(512)]
 
+    for i in range(256):
+        value = input[i]
 
-def decompress_prefetch(data, out):
-    """
-    Description : Decompress the prefetch using LZ77+Huffman Decompression Algorithm
-    Params :
-        @data : The compressed prefetch data extracted from memory
-        @result : The uncompressed prefetch file ready to be forensically analysed
-    Possible errors :
-        Invalid compressed data.
-    """
-    if len(data) < 256:
-        vollog.info("Error : The prefetch must use a 256-byte Huffman table. -> Invalid data")
+        symbolInfo[2*i].id = 2 * i
+        symbolInfo[2*i].symbol = 2 * i
+        symbolInfo[2*i].length = value & 0xf
 
-    # First, we construct our table
-    decoding_table = [0] * (2 ** 15)
-    current_table_entry = 0
-    encoded_data = data[0:256]
-    for bit_length in range(1, 15):
-        for symbol in range(0, 511):
-            if encoded_bit_length(encoded_data,
-                                  symbol) == bit_length:  # If the encoded bit length of symbol equals bit_length
-                entry_count = (1 << (15 - bit_length))
-                for i in range(0, entry_count):
-                    if current_table_entry >= 2 ** 15:  # Huffman table length
-                        vollog.info("The compressed data is not valid.")
-                        return None
-                    decoding_table[current_table_entry] = numpy.uint16(symbol)
-                    current_table_entry += 1
-    if current_table_entry != 2 ** 15:
-        vollog.info("The compressed data is not valid.")
-        exit(1)
+        value >>= 4
 
-    # Then, it's time to decompress the data
-    """
-    The compression stream is designed to be read in (mostly) 16-bit chunks, with a 32-bit register
-    maintaining at least the next 16 bits of input. This strategy allows the code to seamlessly handle the
-    bytes for long match lengths, which would otherwise be awkward.
-    """
-    input_buffer = data
-    current_position = 256  # start at the end of the Huffman table
-    if current_position > len(input_buffer):
-        vollog.info("Incomplete Prefetch")
-        return out
-    next_bits = read_16_bits(input_buffer, current_position)
-    current_position += 2
-    next_bits = numpy.uint32(next_bits) << numpy.int64(16)
-    if current_position > len(input_buffer):
-        vollog.info("Incomplete Prefetch")
-        return out
-    next_bits = next_bits | numpy.uint32(read_16_bits(input_buffer, current_position))
+        symbolInfo[2*i+1].id = 2*i + 1
+        symbolInfo[2*i+1].symbol = 2*i + 1
+        symbolInfo[2*i+1].length = value & 0xf
 
-    current_position += 2
-    extra_bit_count = 16
-    # Loop until a block terminating condition
+    symbolInfo = sorted(symbolInfo, key=lambda x: (x.length, x.symbol))
+
+    i = 0
+    while i < 512 and symbolInfo[i].length == 0:
+        i += 1
+
+    mask = 0
+    bits = 1
+
+    root = treeNodes[0]
+    root.leaf = False
+
+    j = 1
+    while i < 512:
+        treeNodes[j].id = j
+        treeNodes[j].symbol = symbolInfo[i].symbol
+        treeNodes[j].leaf = True
+        mask = mask << (symbolInfo[i].length - bits)
+        bits = symbolInfo[i].length
+        j = prefix_code_tree_add_leaf(treeNodes, j, mask, bits)
+        mask += 1
+        i += 1
+
+    return root
+
+def prefix_code_tree_decode_symbol(bstr: BitStream, root: PREFIX_CODE_NODE) -> Tuple[int, Union[None, Exception]]:
+    node = root
+    i = 0
     while True:
-        next_15_bits = numpy.uint32(next_bits) >> numpy.uint32((32 - 15))
-        huffman_symbol = decoding_table[next_15_bits]
-        huffman_symbol_bit_length = encoded_bit_length(encoded_data, huffman_symbol)
-        next_bits = numpy.int32(next_bits << huffman_symbol_bit_length)
-        extra_bit_count -= huffman_symbol_bit_length
-        if extra_bit_count < 0:
-            if current_position > len(input_buffer):
-                vollog.info("Incomplete Prefetch")
-                return out
-            next_bits = next_bits | (numpy.uint32(read_16_bits(input_buffer, current_position)) << (-extra_bit_count))
-            current_position += 2
-            extra_bit_count += 16
-        if huffman_symbol < 256:
-            out.append(huffman_symbol)
-        elif huffman_symbol == 256 and (len(input_buffer) - current_position) == 0:
-            vollog.info("Decompression is complete")
-            return out
-        else:
-            huffman_symbol = huffman_symbol - 256
-            match_length = huffman_symbol % 16
-            match_offset_bit_length = huffman_symbol // 16
-            if match_length == 15:
-                if current_position > len(input_buffer):
-                    vollog.info("Incomplete Prefetch")
-                    return out
-                match_length = numpy.uint16(read_byte(input_buffer, current_position))
-                current_position += 1
-                if match_length == 255:
-                    if current_position > len(input_buffer):
-                        vollog.info("Incomplete Prefetch")
-                        return out
-                    match_length = read_16_bits(input_buffer, current_position)
-                    current_position += 2
-                    if match_length < 15:
-                        vollog.info("The compressed data is invalid.")
-                        return None
-                    match_length -= 15
-                match_length += 15
-            match_length += 3
-            match_offset = next_bits >> (32 - match_offset_bit_length)
-            match_offset += (1 << match_offset_bit_length)
-            next_bits = next_bits << match_offset_bit_length
-            extra_bit_count -= match_offset_bit_length
-            if extra_bit_count < 0:
-                if current_position > len(input_buffer):
-                    vollog.info("Incomplete Prefetch")
-                    return out
-                next_bits = next_bits | (
-                        numpy.uint32(read_16_bits(input_buffer, current_position)) << (-extra_bit_count))
-                current_position += 2
-                extra_bit_count += 16
-            for i in range(0, int(match_length)):
-                to_write = out[len(out) - int(match_offset)]
-                out.append(to_write)
+        bit = bstr.lookup(1)
+        err = bstr.skip(1)
+        if err is not None:
+            return 0, err
 
+        node = node.child[bit]
+        if node == None:
+            return 0, Exception("Corruption detected")
+
+        if node.leaf:
+            break
+    return node.symbol, None
+
+def lz77_huffman_decompress_chunck(in_idx: int, 
+                                   input: bytes, 
+                                   out_idx: int, 
+                                   output: bytearray, 
+                                   chunk_size: int) -> Tuple[int, int, Union[None, Exception]]:
+    
+    # Ensure there are at least 256 bytes available to read
+    if in_idx + 256 > len(input):
+        return 0, 0, Exception("EOF Error")
+
+    root = prefix_code_tree_rebuild(input[in_idx:])
+    #print_tree(root)
+    bstr = BitStream(input, in_idx+256)
+
+    i = out_idx
+
+    while i < out_idx + chunk_size:
+        symbol, err = prefix_code_tree_decode_symbol(bstr, root)
+        
+        if err is not None:
+            return int(bstr.index), i, err
+        
+        if symbol < 256:
+            output[i] = symbol
+            i += 1
+        else:
+            symbol -= 256
+            length = symbol & 15
+            symbol >>= 4
+
+            offset = 0
+            if symbol != 0:
+                offset = int(bstr.lookup(symbol))
+
+            offset |= 1 << symbol
+            offset = -offset
+
+            if length == 15:
+                length = bstr.source[bstr.index] + 15
+                bstr.index += 1
+                
+                if length == 270:
+                    length = struct.unpack_from('<H', bstr.source, bstr.index)[0]
+                    bstr.index += 2
+
+            err = bstr.skip(symbol)
+            if err is not None:
+                return int(bstr.index), i, err
+            
+            length += 3
+            while length > 0:
+                if i + offset < 0:
+                    print(i + offset)
+                    return int(bstr.index), i, Exception("Decompression Error")
+                
+                output[i] = output[i + offset]
+                i += 1
+                length -= 1
+                if length==0:
+                    break
+    return int(bstr.index), i, None
+
+
+def lz77_huffman_decompress(input: bytes, output_size: int) -> Tuple[bytes, Union[None, Exception]]:
+    output = bytearray(output_size)
+    err = None
+
+    # Index into the input buffer.
+    in_idx = 0
+
+    # Index into the output buffer.
+    out_idx = 0
+
+    while True:
+        # How much data belongs in the current chunk. Chunks
+        # are split into maximum 65536 bytes.
+        chunk_size = output_size - out_idx
+        if chunk_size > 65536:
+            chunk_size = 65536
+
+        in_idx, out_idx, err = lz77_huffman_decompress_chunck(
+            in_idx, input, out_idx, output, chunk_size)
+        if err is not None:
+            return output, err
+        if out_idx >= len(output) or in_idx >= len(input):
+            break
+    return output, None
 
 class Prefetch(interfaces.plugins.PluginInterface):
     """Get and parse the prefetch files"""
@@ -299,7 +379,7 @@ class Prefetch(interfaces.plugins.PluginInterface):
             vollog.info(f"decompressed size : {decompressed_size}")
             stream.seek(0x0008)
             compressed_bytes = stream.read()
-            prefetch_file = decompress_prefetch(bytearray(compressed_bytes), bytearray())
+            prefetch_file = lz77_huffman_decompress(bytearray(compressed_bytes), decompressed_size)[0]
         try:
             file_version = int.from_bytes(prefetch_file[:4], "little")
             signature = prefetch_file[4:8].decode()
@@ -364,7 +444,7 @@ class Prefetch(interfaces.plugins.PluginInterface):
                         bytes_read = 0
                         prefetch_raw = b''
                         try:
-                            for mem_offset, file_offset, datasize in memory_object.get_available_pages():
+                            for mem_offset, _, datasize in memory_object.get_available_pages():
                                 prefetch_raw += layer.read(mem_offset, datasize, pad=True)
                                 bytes_read += len(prefetch_raw)
                                 vollog.info(f"Read {bytes_read}")
